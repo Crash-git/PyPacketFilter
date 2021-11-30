8f2faab4d32f6e0e13f5e724e6dfd73378fb64a9
os274 <82563827+os274@users.noreply.github.com>
2021-11-30 15:39:17 -0500

    Add files via upload

--- Code/filter_packets.py
+++ Code/filter_packets.py
@@ -1,3 +1,29 @@
-def filter() :
-	print('called filter function in filter_packets.py')
-	
+
+#Project 2
+#filtering wiresharks for ICMP packets
+
+
+def filter( filename, L) :
+	f = open( filename + ".txt", 'r' )
+	line = f.readline()
+
+	while "\n" in line:
+		if "ICMP" in line:
+			if ("Echo (ping) reply") in line :
+				splitline = line.strip().split()
+				L.append(splitline)
+				print(splitline)
+			elif ("Echo (ping) request") in line :
+				splitline = line.strip().split()	
+				L.append(splitline)
+				print(splitline)
+		
+		line = f.readline()
+	if "Node1" in filename:
+		writefile = open( filename + "_filter.txt", 'w' )	
+		print("file made")
+
+
+L = []
+filter("Node1", L)
+
