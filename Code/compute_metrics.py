936db3b15a70de151d573f7e50fa5be21727e230
Rowan Keller <r0w4nk3113r@gmail.com>
2021-11-30 15:28:51 -0500

    Tentatively finished data size metric computation

--- Code/compute_metrics.py
+++ Code/compute_metrics.py
@@ -18,22 +18,22 @@ def compute(srcIp, L) : # Computing 13 different metrics per node
         if L[iter][4] = 8: # tracking metrics for echo requests
             if L[iter][1] = srcIp:
                 reqNumSent += 1 # Number of echo requests sent
-                reqSentByte = reqSentByte + L[iter][3] # echo request bytes sent- based on the total size of the frame
-                reqSentData = # echo request bytes sent sent - based on the payload size of the ICMP packet
+                reqSentByte = reqSentByte + (int(L[iter][3]) + 14) # echo request bytes sent- based on the total size of the frame
+                reqSentData = reqSentData + (int(L[iter][3]) - 28) # echo request bytes sent sent - based on the payload size of the ICMP packet
             if L[iter][2] = srcIp:
                 reqRecNum += 1 # Number of echo requests received
-                reqRecByte = reqRecByte + L[iter][3] # echo request bytes received
-                reqRecData =  # echo request data received
+                reqRecByte = reqRecByte + (int(L[iter][3]) + 14) # echo request bytes received
+                reqRecData = reqRecData + (int(L[iter][3]) - 28) # echo request data received
 
         if L[iter][4] = 1: # tracking metrics for echo replies
             if L[iter][1] = srcIp:
                 repNumSent += 1 # Number of echo replies sent
-                repSentByte = repSentByte + L[iter][3] # echo reply bytes sent
-                repSentData =  # echo reply data
+                repSentByte = repSentByte + (int(L[iter][3]) + 14) # echo reply bytes sent
+                repSentData = repSentData + (int(L[iter][3]) - 28) # echo reply data
             if L[iter][2] = srcIp:
                 repRecNum += 1 # Number of echo replies received
-                repRecByte = repRecByte + L[iter][3] # echo reply bytes received
-                repRecData =  # echo reply data received
+                repRecByte = repRecByte + (int(L[iter][3]) + 14) # echo reply bytes received
+                repRecData = repRecData + (int(L[iter][3]) - 28) # echo reply data received
 
 	# Time based metrics
 		# Average Round-Trip-Time, the time between sending an Echo-Req and receiving an Echo-Rep, measured in MS
