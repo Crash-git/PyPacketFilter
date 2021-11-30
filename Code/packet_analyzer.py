e7a562d72623c29b0b99cb5550461d0a25c35523
Rowan Keller <r0w4nk3113r@gmail.com>
2021-11-30 15:28:05 -0500

    Added multiple calls of compute for each node, each call modifies a list that is passed in and uses the IP of the node for metric calculation

--- Code/packet_analyzer.py
+++ Code/packet_analyzer.py
@@ -5,6 +5,13 @@ from compute_metrics import *
 
 # MAIN ------
 L = list()
+M1 = list()
+M2 = list()
+M3 = list()
+M4 = list()
 filter(L)
 parse()
-compute("192.168.100.1", M)
+compute("192.168.100.1", M1)
+compute("192.168.200.2,", M2)
+compute("192.168.300.3,", M3)
+compute("192.168.400.4,", M4)
