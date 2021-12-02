ab9911a5c1b0967a5c8b1d58370aa578d65b60c2
Claire Lavergne <6371640+Crash-git@users.noreply.github.com>
2021-12-02 14:56:36 -0500

    changed parser to run 4 times with a new list

--- Code/packet_parser.py
+++ Code/packet_parser.py
@@ -1,8 +1,9 @@
 #Claire Lavergne
 import sys, csv
 
-def parse(L):
+def parse():
 	print('called parse function in packet_parser.py')
+	L = []
 	
 	#file fields: 0 is num, 1 is time, 2 is source, 3 is dest, 4 is type, 5 is size, 6 is request or reply, 7 is id, 8 is seq, 9 is ttl, 10 is reply spot
 	for i in range(3):
@@ -17,7 +18,7 @@ def parse(L):
 			elif ("Echo (ping) reply" in content[4]):
 				content[4] = "0"
 			L.append(content)
-		#print(L)
-
-L = []
-#parse(L)
+		print(L)
+	return L
+	
+parse()
