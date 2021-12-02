3b09dd0387f8e1010907af6c6602ff9302d86570
Claire Lavergne <6371640+Crash-git@users.noreply.github.com>
2021-12-01 19:51:17 -0500

    made the parser and changed output of filter to .csv

--- Code/filter_packets.py
+++ Code/filter_packets.py
@@ -20,7 +20,7 @@ def filter( filename, L) :
 		
 		line = f.readline()
 
-	writefile = open( filename + "_filter.txt", 'w' )
+	writefile = open( filename + "_filter.csv", 'w' )
 	for iter in L:
    		writefile.write(iter[0] + "," + iter[1] + "," + iter[2] + "," + iter[3] + "," + iter[4] + "," + iter[5]  + "," + iter[6] + " " + iter[7] + " " + iter[8] + "," + iter[9] + iter[10] + iter[11] + "," + iter[12] + " " + iter[13] + " " + iter[14] + "\n")
     
--- Code/packet_parser.py
+++ Code/packet_parser.py
@@ -1,4 +1,19 @@
-def parse() :
-	print('called parse function in packet_parser.py')
+#Claire Lavergne
+import sys, csv
 
+def parse(L):
+	print('called parse function in packet_parser.py')
 	
+	#file fields: 0 is num, 1 is time, 2 is source, 3 is dest, 4 is type, 5 is size, 6 is request or reply, 7 is id, 8 is seq, 9 is ttl, 10 is reply spot
+	for i in range(3):
+		f = open("Node" + str(i+1) + "_filter.csv", 'r')
+		reader = csv.reader(f, delimiter=",")
+		included_cols = [0, 1, 2, 5, 6, 9]
+
+		for row in reader:
+			content = list(row[i] for i in included_cols)
+			L.append(content)
+		print(L)
+
+L = []
+parse(L)
