9da10ad9c65f8b50547ec808a8965d6483ac2120
Claire Lavergne <6371640+Crash-git@users.noreply.github.com>
2021-12-01 20:08:24 -0500

    changed echo request and reply to number codes

--- Code/packet_parser.py
+++ Code/packet_parser.py
@@ -8,10 +8,14 @@ def parse(L):
 	for i in range(3):
 		f = open("Node" + str(i+1) + "_filter.csv", 'r')
 		reader = csv.reader(f, delimiter=",")
-		included_cols = [0, 1, 2, 5, 6, 9]
+		included_cols = [0, 1, 2, 5, 6, 8, 9]
 
 		for row in reader:
 			content = list(row[i] for i in included_cols)
+			if ("Echo (ping) request" in content[4]):
+				content[4] = "8"
+			elif ("Echo (ping) reply" in content[4]):
+				content[4] = "0"
 			L.append(content)
 		print(L)
 
