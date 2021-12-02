433d2fd8f6daa2b33ee6f752cd9847841651b797
Rowan Keller <r0w4nk3113r@gmail.com>
2021-12-01 22:43:19 -0500

    Compute is now functional for data size metrics, given test packets

--- Code/compute_metrics.py
+++ Code/compute_metrics.py
@@ -1,4 +1,5 @@
 def compute(srcIp, L) : # Computing 13 different metrics per node
+    print("compute method called for IP: " + srcIp)
     reqRecNum = 0
     reqSentNum = 0
     reqRecByte = 0
@@ -16,29 +17,27 @@ def compute(srcIp, L) : # Computing 13 different metrics per node
     for iter in range(0, len(L), 1):
 	    # Data size metrics
         #print(str(L[iter][4]))
-        if L[iter][4] == 8 : # tracking metrics for echo requests, index for ICMP type and comparison changed for the test packet
-            if L[iter][1] == srcIp:
-                reqNumSent += 1 # Number of echo requests sent
+        if L[iter][4] == str(8): # tracking metrics for echo requests, index for ICMP type and comparison changed for the test packet
+            if L[iter][2] == srcIp:
+                reqSentNum += 1 # Number of echo requests sent
                 reqSentByte = reqSentByte + (int(L[iter][3]) + 14) # echo request bytes sent- based on the total size of the frame
                 reqSentData = reqSentData + (int(L[iter][3]) - 28) # echo request bytes sent sent - based on the payload size of the ICMP packet
-                print("echo req sent" + str(reqSentNum))
-            if L[iter][2] == srcIp:
+            if L[iter][2] != srcIp:
                 reqRecNum += 1 # Number of echo requests received
                 reqRecByte = reqRecByte + (int(L[iter][3]) + 14) # echo request bytes received
                 reqRecData = reqRecData + (int(L[iter][3]) - 28) # echo request data received
 
-        if L[iter][4] == 0: # tracking metrics for echo replies
-            if L[iter][1] == srcIp:
-                repNumSent += 1 # Number of echo replies sent
+        if L[iter][4] == str(0): # tracking metrics for echo replies
+            if L[iter][2] == srcIp:
+                repSentNum += 1 # Number of echo replies sent
                 repSentByte = repSentByte + (int(L[iter][3]) + 14) # echo reply bytes sent
                 repSentData = repSentData + (int(L[iter][3]) - 28) # echo reply data
-                print("here's a reply" + str(repSentNum))
-            if L[iter][2] == srcIp:
+            if L[iter][2] != srcIp:
                 repRecNum += 1 # Number of echo replies received
                 repRecByte = repRecByte + (int(L[iter][3]) + 14) # echo reply bytes received
                 repRecData = repRecData + (int(L[iter][3]) - 28) # echo reply data received
-                print("Echo rep received")
-
+    print("ECHO REQ - Num Received: " + str(reqRecNum) + ", Num Sent: " + str(reqSentNum) + ", Bytes Received: " + str(reqRecByte))
+    print("ECHO REP - Num Received: " + str(repRecNum) + ", Num Sent: " + str(repSentNum) + ", Bytes Received: " + str(repRecByte))
 	# Time based metrics
 		# Average Round-Trip-Time, the time between sending an Echo-Req and receiving an Echo-Rep, measured in MS
 		# Echo Request Throughput (in kB/s), the sum of the frame sizes of all the Echo-Reqs sent divided by the sum of RTTs
