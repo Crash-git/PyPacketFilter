27d46837dee9d30775da44e00a36fcd56fa3dc79
CrashMan054 <CrashMan054@users.noreply.github.com>
2021-12-05 17:31:09 -0500

    packet parser with dest ip and ttl numbers

--- Code/packet_parser.py
+++ Code/packet_parser.py
@@ -1,5 +1,5 @@
 #Claire Lavergne
-import sys, csv
+import csv
 
 def parse(num):
 	print('called parse function in packet_parser.py')
@@ -8,14 +8,24 @@ def parse(num):
 	#file fields: 0 is num, 1 is time, 2 is source, 3 is dest, 4 is type, 5 is size, 6 is request or reply, 7 is id, 8 is seq, 9 is ttl, 10 is reply spot
 	f = open("Node" + str(num) + "_filter.csv", 'r')
 	reader = csv.reader(f, delimiter=",")
-	included_cols = [0, 1, 2, 5, 6, 8, 9]
+	included_cols = [0, 1, 2, 3, 5, 6, 8, 9]
 
 	for row in reader:
 		content = list(row[i] for i in included_cols)
-		if ("Echo (ping) request" in content[4]):
-			content[4] = "8"
-		elif ("Echo (ping) reply" in content[4]):
-			content[4] = "0"
+		#echo to number
+		if ("Echo (ping) request" in content[5]):
+			content[5] = "8"
+		elif ("Echo (ping) reply" in content[5]):
+			content[5] = "0"
+		
+		#ttl to number
+		ttl = content[7]
+		res = [int(i) for i in ttl.split('=') if i.isdigit()]
+		content[7] = str(res[0])
 		L.append(content)
+
 	print(L)
 	return L
+
+#debug
+#parse(1)
